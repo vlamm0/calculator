e0720b5d72f3d8e1b61d6fe726008866f6b2cca4
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-13 11:20:56 -0400

    Completes operations based on user input in console

--- code.js
+++ code.js
@@ -13,3 +13,32 @@ function multiply(x,y) {
 function divide(x,y) {
     return x/y;
 }
+
+// calls correct operation based on user input
+function operate(arr) {
+    let operand1 = Number(arr[0])
+    let operand2 = Number(arr[2])
+    let operation = arr[1];
+    switch (operation) {
+        case "+":
+            return add(operand1,operand2);
+            break;
+        case "-":
+            return subtract(operand1,operand2);
+            break;
+        case "*":
+            return multiply(operand1,operand2);
+            break;
+        case "/":
+            return divide(operand1,operand2);
+            break;
+        default:
+            alert("not a valid operator")
+        
+    }
+}
+
+var query = prompt("query")
+query = query.split(" ")
+// send query results through operate to call correct operation
+alert(operate(query))
